42fa83f0a30c558d7a21c29022ff4c2e2c1a9445
Juxn09 <145414678+Juxn09@users.noreply.github.com>
2025-03-12 22:07:48 -0500

    Add files via upload

---
+++ ListaDoble/src/Pila.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+
+public class Pila<T> extends ListaDoble<T> {
+
+    public Pila(){
+        super();
+    }
+
+    // Empty
+    public boolean empty(){
+        return cantidadNodos == 0;
+    }
+    public T peek(){
+        return head.getSig().getDato();
+    }
+    public T pop(){
+        return super.remove();
+    }
+
+    public  T push(T dato){
+      super.remove();
+      return dato;
+    }
+
+    public int search(T dato){
+        Nodo<T> nodo = head;
+        for (int i = 0; i <cantidadNodos ; i++) {
+            nodo= nodo.getSig();
+            if (Objects.equals(nodo.getDato(),dato)){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
